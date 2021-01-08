3e4800ea42e4ff74650ac48e98ec130d103aacfe
DNoki <490889409@qq.com>
2021-01-08 16:42:46 +0900

    项目拆分1

--- OpenGL_Learn/Main.cpp
+++ OpenGL_Learn/Main.cpp
@@ -1,6 +1,4 @@
-﻿#pragma once
-
-#include <Windows.h>
+﻿#include <Windows.h>
 #include <iostream>
 #include <memory>
 #include <vector>
@@ -10,38 +8,9 @@
 
 using namespace std;
 
-#include "GameSystem.h"
+#include "../OpenGL_Core/GameSystem.h"
 
 using namespace OpenGL_Learn;
-
-class Father
-{
-public:
-    virtual void DoFun()
-    {
-        cout << "Father" << endl;
-    }
-
-private:
-
-};
-
-class Child : public Father
-{
-public:
-    void DoFun() override
-    {
-        cout << "Child" << endl;
-    }
-
-    ~Child()
-    {
-        cout << "Child Destruct" << endl;
-    }
-
-private:
-
-};
 
 void TestFunction(bool (*func_ptr)(const int&))
 {
